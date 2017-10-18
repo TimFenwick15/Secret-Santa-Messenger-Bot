a2381614804b3a76a862e214372d785464e69c73
TimFenwick15 <tjf20@hotmail.co.uk>
2017-10-18 21:06:24 +0100

    more placeholder for test

--- secretSantaPairings.test.js
+++ secretSantaPairings.test.js
@@ -1,7 +1,37 @@
 const santa = require('./secretSantaPairings')
 const assert = require('assert')
-describe('it', function() {
+const data = require('./data')
+describe('convertExcludesToIncludes', function() {
   it('should work', function() {
+    //santa.convertExcludesToIncludes
+    assert.equal(1, 1)
+  })
+})
+
+describe('addArrayLengthToKey', function() {
+  it('should work', function() {
+    //santa.addArrayLengthToKey
+    assert.equal(1, 1)
+  })
+})
+
+describe('sortPeopleByKeyNumber', function() {
+  it('should work', function() {
+    //santa.sortPeopleByKeyNumber
+    assert.equal(1, 1)
+  })
+})
+
+describe('makePairings', function() {
+  it('should work', function() {
+    //santa.makePairings
+    assert.equal(1, 1)
+  })
+})
+
+describe('sendMessages', function() {
+  it('should work', function() {
+    //santa.sendMessages
     assert.equal(1, 1)
   })
 })
